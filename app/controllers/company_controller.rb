de00ffeff8af9f17be62a5e624c7655ef7b77399
Gary <garygrant693@gmail.com>
2021-01-30 15:02:29 -0500

    added controller logic to Company controller

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -2,16 +2,25 @@ class CompanyController < ApplicationController
   def index
      #the index method is a get request that will retreive all
     #the requested data in our database
+    @company = Company.all
+    render json: @company
   end
 
   def show
     #the show method will return one specific item from the 
     #database with the use of an ID (it is also a get request)
+    render json: @company
   end
 
   def create
    #create is a post request that will create data that is submitted
     #from the client side to the backend(create will not take an ID)
+    @company = Company.new(company_params)
+    if @company.save
+      render json: @company, status: :created
+    else
+      render json: @company, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -19,6 +28,11 @@ class CompanyController < ApplicationController
     #the update method will first use the find method to locate the
     #item then use the update method to change the state of the item
     #update does use an ID
+    if @company.update(company_params)
+      render json: @company
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
   end
 
   def delete
@@ -27,5 +41,12 @@ class CompanyController < ApplicationController
     #destroy method it must first use the find method to locate
     #the item in the database.Once the item is selected it can
     #select and destroy the item from the database
+    @company.destroy
+  end
+
+  private
+
+  def company_params
+    @company = Company.find(params[:id])
   end
 end

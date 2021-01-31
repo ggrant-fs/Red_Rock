eb952d7061810e0caf539c2be6e750bb9a3cda8d
Gary <garygrant693@gmail.com>
2021-01-31 01:52:01 -0500

    created password validations in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username , presence:true, uniqueness: true
+    validates :email, presence:true, uniqueness: true
+    validates :email, format: { with: URI:MailTo::EMAIL_REGEX}
+    validates :password, length: {minimum: 6}
 end

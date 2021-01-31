35ad5ed94daec9a2705c73923d049e581f5733fe
Gary <garygrant693@gmail.com>
2021-01-31 02:55:10 -0500

    added authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'user/create'
   get 'user/update'
   get 'user/delete'
+  get '/auth/verify', to: 'authentication#verify'
+  post '/auth/login', to: 'authentication#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources:user
   resources:company

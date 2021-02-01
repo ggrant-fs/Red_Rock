413d4f4645da85cfacdeb98b92213592caa29f9d
Gary <garygrant693@gmail.com>
2021-02-01 18:18:37 -0500

    made edits to company and user controller

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,35 +1,29 @@
 class CompanyController < ApplicationController
-  before :set_user, only: [:show, :update, :delete]
+  before_action :set_user, only: [:show, :update, :delete]
   before_action :authorize_request, except: :create
   def index
-     #the index method is a get request that will retreive all
-    #the requested data in our database
     @company = Company.all
     render json: @company
   end
 
   def show
-    #the show method will return one specific item from the 
-    #database with the use of an ID (it is also a get request)
     render json: @company
   end
 
   def create
-   #create is a post request that will create data that is submitted
-    #from the client side to the backend(create will not take an ID)
     @company = Company.new(company_params)
     if @company.save
-      render json: @company, status: :created
-    else
-      render json: @company, status: :unprocessable_entity
-    end
+      @token = encode({id:@company.id})
+     render json: {
+       company: @company.attributes.except("password_digest"),
+       token: @token
+     }, status: :created
+     else
+      render json: @company.errors, status: :unprocessable_entity
+     end
   end
 
   def update
-    #udate is a put request to update an item in our database
-    #the update method will first use the find method to locate the
-    #item then use the update method to change the state of the item
-    #update does use an ID
     if @company.update(company_params)
       render json: @company
     else
@@ -38,17 +32,17 @@ class CompanyController < ApplicationController
   end
 
   def delete
-      #The delete meothod will use a destroy request. Although
-    #before an item is removed permently from the database via the
-    #destroy method it must first use the find method to locate
-    #the item in the database.Once the item is selected it can
-    #select and destroy the item from the database
     @company.destroy
   end
 
   private
 
+def set_company
+  @company = Company.find(params[:id])
+end
+
   def company_params
     @company = Company.find(params[:id])
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,16 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :delete]
   before_action :authorize_request, except: :create
-  #controllers provides us with all the logic as to 
-  #how the routes are managed. In ruby to create the 
-  # routes we must first create the controlers with 
-  # the following methods (index, show, create, update
-  # delete)
-
-  #each of these methods will have specifi logic what will 
-  #satisfy CRUD
-  def index
-    #the index method is a get request that will retreive all
-    #the requested data in our database
-   
-    # @user is an instance variable that holds our User get request
-    # line 17 renders data in the variable in json format
-    @user = User.all
+ 
+  def index 
     render json @user
-  
-   
   end
 
   def show
-    #the show method will return one specific item from the 
-    #database with the use of an ID (it is also a get request)
     render json: @user
   end
 
   def create
-    #create is a post request that will create data that is submitted
-    #from the client side to the backend(create will not take an ID)
     @user = User.new(user_params)
       if @user.save
         @token = encode({id:@user.id})
@@ -44,10 +25,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    #udate is a put request to update an item in our database
-    #the update method will first use the find method to locate the
-    #item then use the update method to change the state of the item
-    #update does use an ID
     if @user.update(user_params)
       render json: @user
     else
@@ -56,11 +33,6 @@ class UsersController < ApplicationController
   end
 
   def delete
-    #The delete meothod will use a destroy request. Although
-    #before an item is removed permently from the database via the
-    #destroy method it must first use the find method to locate
-    #the item in the database.Once the item is selected it can
-    #select and destroy the item from the database
     @user.destroy 
   end
 

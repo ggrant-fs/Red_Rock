8513cda12cd7419d6c372061e5fa7fc3722679f1
Gary <garygrant693@gmail.com>
2021-02-01 15:22:52 -0500

    Register form completed

--- client/src/Componenet/Register/Register.jsx
+++ client/src/Componenet/Register/Register.jsx
@@ -9,9 +9,64 @@ const Register = () => {
         suffix: '',
         duns: ''
     })
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setNewProfile((prevState) => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefualt()
+    }
     return (
         <React.Fragment>
-
+            <form>
+                <input
+                    type='text'
+                    name='username'
+                    value={newProfile.username}
+                    onChange={handleChange}
+                    placeholder='Username...'
+                />
+                <input
+                    type='email'
+                    name='email'
+                    value={newProfile.email}
+                    onChange={handleChange}
+                    placeholder='Email...'
+                />
+                <input
+                    type='text'
+                    name='phone_number'
+                    value={newProfile.phone_number}
+                    onChange={handleChange}
+                    placeholder='Phone #'
+                />
+                <input
+                    type='text'
+                    name='Industry'
+                    value={newProfile.industry}
+                    onChange={handleChange}
+                    placeholder='Industry...'
+                />
+                <input
+                    type='text'
+                    name='Suffix'
+                    value={newProfile}
+                    onChange={handleChange}
+                    placeholder='i.e. LLC'
+                />
+                <input
+                    type='text'
+                    name='duns'
+                    value={newProfile.duns}
+                    onChange={handleChange}
+                    placeholder='Duns number'
+                />
+            </form>
         </React.Fragment>
     );
 };

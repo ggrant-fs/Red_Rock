1566ad3bab95c3f7a3e6354d359ccc842ed6804d
Gary <garygrant693@gmail.com>
2021-02-01 15:26:39 -0500

    created MainContainer.jsx

--- client/src/Componenet/Register/Register.jsx
+++ client/src/Componenet/Register/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     }
     return (
         <React.Fragment>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type='text'
                     name='username'
@@ -66,6 +66,7 @@ const Register = () => {
                     onChange={handleChange}
                     placeholder='Duns number'
                 />
+                <butto type="submit">Submit</butto>
             </form>
         </React.Fragment>
     );

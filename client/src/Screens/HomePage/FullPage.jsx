36aa6397f5ca56e42caa88c21c62c01d9d985290
Gary <garygrant693@gmail.com>
2021-02-03 22:30:22 -0500

    added written content to section 5 home screen

--- client/src/Screens/HomePage/FullPage.jsx
+++ client/src/Screens/HomePage/FullPage.jsx
@@ -23,10 +23,23 @@ const Fullpage = () => (
                                 <Link to='/register' className='nav-link'><li>Join us</li></Link>
                             </nav>
                         </div>
+
                         <div>
-                            <h1 id='title'>Enter</h1>
-                            <h1>s</h1>
+                            <h1 id='sec-one-intro'>Life Lavish <span id='sec-one-span'>and</span> Luxury Elite</h1>
+                            <div id='container-logos'>
+                                <div id='cnn-logo'>
+                                    <img id='bbc-logo' alt='' src='https://fontmeme.com/permalink/210203/4812477f0573952bfd586c03abf0105b.png' />
+                                </div>
+                                <div id='ny-times-logo'>
+                                    <img alt='' src='https://i.imgur.com/Ljnfyt4.png' />
+                                </div>
+                                <div>
+                                    <img alt='' src='https://fontmeme.com/permalink/210203/76071269cdf21135fa2ab7f883e971c5.png' />
+                                </div>
+                            </div>
                         </div>
+
+
                     </div>
                     <div id='background-two' className="section">
                         <div id='nav-container2'>
@@ -40,6 +53,22 @@ const Fullpage = () => (
                         </div>
                         <div id='split'>
                             <h1 id='section-two-title'>Luxury Concierge</h1>
+                            <p id='sec-two-content'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                                aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                            </p>
+                            <p id='sec-two-content-2'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                                aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                            </p>
                         </div>
 
                     </div>
@@ -53,7 +82,9 @@ const Fullpage = () => (
                                 <Link to='/register' className='nav-link'><li>Join us</li></Link>
                             </nav>
                         </div>
-                        <p>Section 3</p>
+                        <div>
+                            {/* <h1 id='sec-3-h1'>The Most Lavishing<br /> Experience</h1> */}
+                        </div>
                     </div>
                     <div id='background-four' className="section">
                         <div id='nav-container4'>
@@ -64,8 +95,8 @@ const Fullpage = () => (
                                 <Link to='/login' className='nav-link'><li>Login</li></Link>
                                 <Link to='/register' className='nav-link'><li>Join us</li></Link>
                             </nav>
+
                         </div>
-                        <p>Section 4</p>
                     </div>
                     <div id='background-five' className="section">
                         <div id='nav-container5'>
@@ -76,9 +107,33 @@ const Fullpage = () => (
                                 <Link to='/login' className='nav-link'><li>Login</li></Link>
                                 <Link to='/register' className='nav-link'><li>Join us</li></Link>
                             </nav>
+                            <div id='split-2'>
+                                <h1 id='section-five-title'>The Lifestyle</h1>
+                                <p className='sec-five-content'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                    officia deserunt mollit anim id est laborum.
+                                </p>
+                                <p className='sec-five-content'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                                    {/* nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                    officia deserunt mollit anim id est laborum. */}
+                                </p>
+                                <p className='sec-five-content'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
                         </div>
-                        <p>Section </p>
-                        <button onClick={() => fullpageApi.moveSectionDown()}></button>
+
+                        {/* <button onClick={() => fullpageApi.moveSectionDown()}></button> */}
                     </div>
                 </ReactFullpage.Wrapper>
             );
